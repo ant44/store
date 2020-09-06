e4e048d1dcdbf98aef52344b017496b42e965f9b
hamzeh.v <dev.pharaoh.ant@gmail.com>
2020-09-06 13:23:03 +0430

    make product table

---
+++ database/migrations/2020_09_06_084141_create_product_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('topic');
+            $table->text('description');
+            $table->string('price');
+            $table->string('discount_price')->nullable();
+            $table->string('discount')->nullable();
+            $table->integer('category_id');
+            $table->string('image_url')->nullable();
+            $table->integer('count')->nullable();
+            $table->integer('size')->nullable();
+            $table->integer('weight')->nullable();
+            $table->integer('seller_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
