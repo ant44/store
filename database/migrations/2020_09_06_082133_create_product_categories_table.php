cde2a347caa52b0703c1a19199b0cea47477067a
hamzeh.v <dev.pharaoh.ant@gmail.com>
2020-09-06 13:07:24 +0430

    make table product_categories and make database

---
+++ database/migrations/2020_09_06_082133_create_product_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('topic');
+            $table->text('description')->nullable();
+            $table->integer('praent_id');
+            $table->string('image_url')->nullable();
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+}
